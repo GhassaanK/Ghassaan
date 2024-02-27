9ff038dfc8c5947ee1ccd23bebec042a30032598
Ghassaan Khalid <ghassaank1@gmail.com>
2024-02-27 23:12:13 +0500

    Fixed alt tags in Team.jsx

--- src/Comps/Team.jsx
+++ src/Comps/Team.jsx
@@ -10,21 +10,21 @@ const Team = () => {
       <div className="card-container">
 
         <div className="card">
-          <img className="card-img-top images" src={anas} alt="Card image cap" />
+          <img className="card-img-top images" src={anas} alt="Card cap" />
           <div className="card-body">
             <p className="card-text">Anas</p>
           </div>
         </div>
 
         <div className="card">
-          <img className="card-img-top images" src={ghassaan} alt="Card image cap" />
+          <img className="card-img-top images" src={ghassaan} alt="Card cap" />
           <div className="card-body">
             <p className="card-text">Ghassaan (Me)</p>
           </div>
         </div>
 
         <div className="card">
-          <img className="card-img-top images" src={wajdan} alt="Card image cap" />
+          <img className="card-img-top images" src={wajdan} alt="Card cap" />
           <div className="card-body">
             <p className="card-text">Wajdan</p>
           </div>
